ad6364abc3cfcc716e730297bc593190eae8c58c
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-07-30 20:36:34 +0900

    Fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react'
-import Navigation from './Navigation'
+// import * as React from 'react'
+// import Navigation from './Navigation'
 
 const Footer = () => {
   return (
-    <footer id={ 'footer' } className={ 'footer' }>
-      <h2 className={ 'footer-title' }>{ 'footer' }</h2>
-      <Navigation prefix={ 'footer' } />
-    </footer>
+    null
+    // <footer id={ 'footer' } className={ 'footer' }>
+    //   <h2 className={ 'footer-title' }>{ 'footer' }</h2>
+    //   <Navigation prefix={ 'footer' } />
+    // </footer>
   )
 }
 
